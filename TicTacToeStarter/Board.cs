44e40bc0b5c894ac3fbd1dc289fe1f05b020588a
mariaigs <2331016@champlaincollege.qc.ca>
2024-11-06 11:29:50 -0500

    Hey

--- TicTacToeStarter/Board.cs
+++ TicTacToeStarter/Board.cs
@@ -8,6 +8,17 @@ namespace TicTacToeStarter
 {
     internal class Board
     {
-        
+        PlayerEnum[,] board = new PlayerEnum[3, 3];
+
+        public Board()
+        {
+            for(int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    board[x, y] = PlayerEnum.NONE;
+                }
+            }
+        }
     }
 }
